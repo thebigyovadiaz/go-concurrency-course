8331289508cd6988214ee0e78b0ff7f34f449052
YovaDiaz <thebigyovadiaz@gmail.com>
2025-11-08 00:41:10 -0300

    Add resizing images

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	fpg "github.com/thebigyovadiaz/go-concurrency-course/code-challenge/fixed-pool-goroutines"
 	grg "github.com/thebigyovadiaz/go-concurrency-course/code-challenge/getting-results-goroutines"
 	tc "github.com/thebigyovadiaz/go-concurrency-course/code-challenge/timeout-cancellations"
 	wg "github.com/thebigyovadiaz/go-concurrency-course/code-challenge/waiting-goroutines"
@@ -10,4 +11,5 @@ func main() {
 	wg.ExecTimingHTTPCalls()
 	grg.ExecValidateSigs()
 	tc.ExecNextMovie()
+	fpg.ExecCenterDiv()
 }
